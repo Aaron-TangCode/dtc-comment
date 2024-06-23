980d1538d98e6f00b3c9c23558bb02ed69c05eff
bigheadcai <tanghailin@58.com>
2024-06-23 23:43:17 +0800

    评论接口开发

--- dtc-comment-controller/src/main/java/com/datoucai/controller/CommentController.java
+++ dtc-comment-controller/src/main/java/com/datoucai/controller/CommentController.java
@@ -1,33 +1,43 @@
 package com.datoucai.controller;
 
-import com.datoucai.service.IUserService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.datoucai.param.AddCommentRequestParam;
+import com.datoucai.param.BaseResult;
+import com.datoucai.param.CommentResultParam;
+import com.datoucai.param.DelCommentRequestParam;
+import com.datoucai.param.QueryCommentRequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 评论Controller
- */
 @RestController
 @RequestMapping("/comment")
-@Slf4j
 public class CommentController {
 
-//    Logger log = LoggerFactory.getLogger(CommentController.class);
-    @Autowired
-    private IUserService userService;
+    /**
+     * 增加评论
+     * @param param
+     * @return
+     */
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    public BaseResult<Boolean> addComment(AddCommentRequestParam param){
+        return null;
+    }
+    /**
+     * 删除评论
+     * @param param
+     * @return
+     */
+    @RequestMapping(value = "/delete",method = RequestMethod.POST)
+    public BaseResult<Boolean> deleteComment(DelCommentRequestParam param){
+        return null;
+    }
+    /**
+     * 查询评论
+     * @param param
+     * @return
+     */
     @RequestMapping(value = "/query",method = RequestMethod.GET)
-    public String query(){
-        int userTotal = userService.countUserTotal();
-//        trace<  debug < info < warn < error
-        log.trace("trace级别日志");
-        log.debug("debug级别日志");
-        log.info("info级别日志");
-        log.warn("warn级别日志");
-        log.error("error级别日志");
-        // 查询一下用户总人数
-        return "大头菜-用户总人数:"+userTotal;
+    public BaseResult<CommentResultParam> queryComment(QueryCommentRequestParam param){
+        return null;
     }
 }
---
+++ dtc-comment-controller/src/main/java/com/datoucai/param/QueryCommentRequestParam.java
@@ -0,0 +1,49 @@
+package com.datoucai.param;
+
+import lombok.Data;
+
+/**
+ * 增加评论
+ */
+@Data
+public class QueryCommentRequestParam {
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 模块
+     * 0：社区模块
+     * 1：游戏模块
+     * 2：短视频模块"
+     */
+    private Integer module;
+    /**
+     * 资源id
+     */
+    private String resourceId;
+    /**
+     * 评论
+     */
+    private Integer score;
+    /**
+     * 排序方式
+     * 1：最新
+     * 2：最热
+     * 3：最早
+     */
+    private Integer order;
+    /**
+     * 子回复数量
+     */
+    private Integer replyNum;
+    /**
+     * 页码
+     */
+    private Integer pageNum;
+    /**
+     * 页大小
+     */
+    private Integer pageSize;
+}
