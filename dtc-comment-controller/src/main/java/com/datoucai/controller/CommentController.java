4e3e5e288a7a3cdeffc975d5367448a04f355138
bigheadcai <tanghailin@58.com>
2024-06-23 23:12:17 +0800

    集成日志框架

--- dtc-comment-controller/src/main/java/com/datoucai/controller/CommentController.java
+++ dtc-comment-controller/src/main/java/com/datoucai/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.datoucai.controller;
 
 import com.datoucai.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/comment")
+@Slf4j
 public class CommentController {
 
-
+//    Logger log = LoggerFactory.getLogger(CommentController.class);
     @Autowired
     private IUserService userService;
     @RequestMapping(value = "/query",method = RequestMethod.GET)
     public String query(){
         int userTotal = userService.countUserTotal();
+//        trace<  debug < info < warn < error
+        log.trace("trace级别日志");
+        log.debug("debug级别日志");
+        log.info("info级别日志");
+        log.warn("warn级别日志");
+        log.error("error级别日志");
         // 查询一下用户总人数
         return "大头菜-用户总人数:"+userTotal;
     }
